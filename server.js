90ac00f2fb57d0808ecb91cf9181a81d270c976e
mberkesaritas <berkesaritas4458@gmail.com>
2022-04-29 13:17:00 +0300

    Web Scoket ChatApp

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const socket = require("socket.io")
+
+const app = express()
+const server = app.listen(3000)
+
+app.use(express.static("./"))
+
+const io = socket(server)
+
+io.on("connection" , (socket) => {
+  console.log(socket.id)
+
+  socket.on("chat" , (data) =>{
+    console.log(data)
+    io.sockets.emit("chat" , data)
+  })
+
+  socket.on("typing" ,(data) => {
+    socket.broadcast.emit("typing" , data)
+  } )
+}) 
